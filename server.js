ae2185562f7fafc58163d68cfa3c63f74a46fb49
bradwhist <bradwhist@gmail.com>
2018-08-05 07:15:27 -0700

    passport

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session    = require('express-session');
 const passport   = require('passport');
 const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 const axios      = require('axios');
+let ws;
 mongoose.connect(process.env.MONGODB_URI);
 
 const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
@@ -45,6 +46,35 @@ OAuth2Strategy.prototype.userProfile = function(accessToken, done) {
 });
 }
 
+app.get('/auth/twitch', passport.authenticate('twitch', { scope: 'user_read' }));
+
+// Set route for OAuth redirect
+app.get('/auth/twitch/callback', passport.authenticate('twitch', { successRedirect: '/', failureRedirect: '/' }));
+
+
+// function heartbeat() {
+//     message = {
+//         type: 'PING'
+//     };
+//     $('.ws-output').append('SENT: ' + JSON.stringify(message) + '\n');
+//     ws.send(JSON.stringify(message));
+// }
+//
+// function connect() {
+//   var heartbeatInterval = 1000 * 60; //ms between PING's
+//   var reconnectInterval = 1000 * 3; //ms to wait before reconnect
+//   var heartbeatHandle;
+//
+//   ws.onmessage = function(event) {
+//     message = JSON.parse(event.data);
+//     $('.ws-output').append('RECV: ' + JSON.stringify(message) + '\n');
+//     if (message.type == 'RECONNECT') {
+//         $('.ws-output').append('INFO: Reconnecting...\n');
+//         setTimeout(connect, reconnectInterval);
+//     }
+// };
+//
+// ws = new WebSocket('wss://pubsub-edge.twitch.tv');
 
 
 const PORT = process.env.PORT || 3000;
