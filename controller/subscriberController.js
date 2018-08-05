22c9047dc9baaf3bb9d0bf87731ab1a32b4a64ad
Emmanuel <emmanueljucaban@gmail.com>
2018-08-05 02:24:44 -0700

    Update jukebox

--- controller/subscriberController.js
+++ controller/subscriberController.js
@@ -4,18 +4,18 @@ const Broadcaster = require('./../model').Broadcaster;
 module.exports = {
   create: async (req, res) => {
     try {
-      const broadcaster = await Broadcaster.findById(req.body.id);
-      const newSub = new Subscriber({
+      let broadcaster = await Broadcaster.findById(req.body.casterId);
+      let newSub = new Subscriber({
         username      : req.body.username,
         subId         : req.body.subId,
         contribution  : 0
       });
-
-
-
-
-
+      newSub = await newSub.save();
+      broadcaster.subscribers.push(newSub);
+      broadcaster = await broadcaster.save();
+      res.json(broadcaster);
+    } catch(e) {
+      res.json(e);
     }
-
   }
 }
--- model/Jukebox.js
+++ model/Jukebox.js
@@ -3,8 +3,8 @@ const Schema   = mongoose.Schema;
 
 const JukeboxSchema = new Schema({
   tracks             : [{type: Schema.Types.ObjectId, ref: 'Track'}],
+  contributors       : [{type: Schema.Types.ObjectId, ref: 'Subscriber'}],
   totalContributions : Number,
-  contributors       : [{type: Schema.Types.ObjectId, ref: 'Subscriber'}]
 });
 
 module.exports = mongoose.model('Jukebox', JukeboxSchema);
