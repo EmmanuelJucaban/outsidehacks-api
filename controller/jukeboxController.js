4d7e03d36487fd5f867b9b70de6a7001ce8d5c4b
bradwhist <bradwhist@gmail.com>
2018-08-05 09:39:02 -0700

    adding auction route

--- controller/jukeboxController.js
+++ controller/jukeboxController.js
@@ -5,56 +5,48 @@ const Jukebox      = require('./../model').Jukebox;
 module.exports = {
   findOne: async (req, res) => {
     try {
-      const jukebox = await Jukebox.findById(req.params.id).populate('tracks');
+      const jukebox = await Jukebox.findOne(req.body.id);
       res.json(jukebox)
     } catch(e) {
       res.json(e);
     }
   },
   addTrack: async (req, res) => {
+    const newSong = new Track({
+      artist  : req.body.artist,
+      title   : req.body.title,
+      trackId : req.body.trackId
+    });
 
 
 
     try {
-      let newSong = new Track({
-        artist   : req.body.artist,
-        title    : req.body.title,
-        trackId  : req.body.trackId,
-        duration : req.body.duration,
-      });
-      const jukebox = await Jukebox.findById(req.body.casterId).populate('tracks');
-      console.log(jukebox);
-      newSong = await newSong.save();
-      jukebox.tracks.push(newSong);
-      const user = jukebox.contributors.find(user => user._id === req.body.userId);
-      console.log(user);
-      if(user) {
-        console.log("Fired at use");
-        user.contribution = user.contribution + 1;
-        jukebox.totalContributions += user.contribution;
-        await jukebox.save();
-        console.log(jukebox);
-        res.json(jukebox.populate('tracks'));
-      }
-      if(!user) {
-        console.log("fired");
-        jukebox.contributors.push({_id: req.body.userId, contribution: 1});
-        jukebox.totalContributions++;
-        await jukebox.save();
-        console.log(jukebox);
-        res.json(jukebox.populate('tracks'));
-      }
-
-
-
+      await newSong.save();
+      const jukebox = await jukebox.findbyId(req.body.id);
+      jukebox.push(newSong);
+      await jukebox.save();
+      res.json(jukebox);
     } catch(e) {
       res.json(e);
     }
   },
-  deleteTrack: async (req, res) => {
-    const jukebox = await Jukebox.findById(req.body.jukeboxId).populate('tracks');
-    jukebox.tracks.shift();
-    await jukebox.save();
-    res.json(jukebox.tracks);
-  }
+  auctionTrack: async (req, res) => {
+    const newSong = new Track({
+      artist  : req.body.artist,
+      title   : req.body.title,
+      trackId : req.body.trackId
+    });
+
+
+
+    try {
+      await newSong.save();
+      const jukebox = await jukebox.findbyId(req.body.id);
+      jukebox.splice(1, 0, newSong);
+      await jukebox.save();
+      res.json(jukebox);
+    } catch(e) {
+      res.json(e);
+    }
+  },
 };
