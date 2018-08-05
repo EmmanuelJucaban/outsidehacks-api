1cd54d7b473df6fa4f948d1bc8697af5662c8595
Emmanuel <emmanueljucaban@gmail.com>
2018-08-05 03:10:43 -0700

    Fix typo in controller

--- route/api/index.js
+++ route/api/index.js
@@ -1,12 +1,12 @@
 const router              = require('express').Router();
 const jukeboxRoutes       = require('./jukebox');
-const broadcasterRoutes   = require('./broadcaster');
-const subscriberRoutes    = require('./subscriber');
+// const broadcasterRoutes   = require('./broadcaster');
+// const subscriberRoutes    = require('./subscriber');
 const trackRoutes         = require('./track')
 
 router.use('/jukebox', jukeboxRoutes);
-router.use('/track', trackRoutes);
+// router.use('/track', trackRoutes);
 router.use('/broadcaster', broadcasterRoutes);
-router.use('/subscriber', subscriberRoutes);
+// router.use('/subscriber', subscriberRoutes);
 
 module.exports = router;
